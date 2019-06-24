7f89f5d1811c4e40dd51076b8a4d831732cc4c02
Robert Root <rmroot@live.com>
2019-06-23 21:33:42 -0500

    final toasty animation example

--- src/app/core/animations-example/animations-example.module.ts
+++ src/app/core/animations-example/animations-example.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AnimationsExampleComponent } from './animations-example.component';
-
 @NgModule({
   declarations: [AnimationsExampleComponent],
-  imports: [
-    CommonModule,
-    BrowserAnimationsModule
-  ]
+  imports: [CommonModule, BrowserAnimationsModule]
 })
 export class AnimationsExampleModule { }
--- src/app/core/animations-example/toast-animation.ts
+++ src/app/core/animations-example/toast-animation.ts
@@ -1,9 +1,12 @@
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { trigger, state, style, animate, transition, AnimationTriggerMetadata } from '@angular/animations';
 
-export const toastAnimation = trigger('toast',
+export const toastAnimation: AnimationTriggerMetadata = trigger('toast',
     [
         state('show', style({
             bottom: '40px'
+        })),
+        state('hide', style({
+            bottom: '-100%'
         })),
         transition('hide => show', animate('1s ease')),
         transition('show => hide', animate('1s ease'))
